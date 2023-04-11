039b428fe54b0691140316ce9ac4a41d0d16ab09
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-11 23:11:26 +0100

    Trying to understand the working environment of react

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-// function App() {
-//   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-    <h1>This is a declarative way of write React</h1>
-  // );
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 
-// }
+}
 
-// export default App;
+export default App;
 
 class Car {
   constructor(name) {
@@ -35,3 +34,4 @@ class Car {
 const mycar = new Car("Ford")
 
 document.write(mycar.brand)
+
