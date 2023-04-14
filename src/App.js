ae742176dbf42f87d55565700d7698d03a06fe5e
Jeremiah Ikechukwu Egbune <jegbune1@gmail.com>
2023-04-15 00:18:20 +0100

    React lesson on UI design

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function MainContent() {
       <img src="./logo512.png"  alt='logo' />
       </nav>
        <ul className='nav-list'>
-          <li>Menu</li>
+          <li>Pricing</li>
           <li>About</li>
           <li>Contact</li>
         </ul>
